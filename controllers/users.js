bd2d435d9ab2dcbb9b05f340a395e5b79492daa3
Khabirova Evelina <evelina.khabirova@yandex.ru>
2022-10-08 18:20:03 +0300

    Поправить обработчик ошибок. Тест 5

--- controllers/users.js
+++ controllers/users.js
@@ -1,90 +1,32 @@
 const Users = require('../models/users');
 
 module.exports.getUsers = (req, res) => {
-  try {
-    const users = Users.find({});
-    res.status(200).send(users);
-  }
-  catch (err) {
-    if (err.name === 'ValidatorError') {
-      res.status(400).send({message: 'Введены неверные данные'});
-      return;
-    }
-    res.status(500).send({message: err.message});
-  }
+  Users.find({})
+  .then(users => res.send({data: users}))
+  .catch(() => res.status(500).send({message: 'Произошла ошибка'}));
 };
 
 module.exports.getProfile = (req, res) => {
-  try {
-    const user = Users.findById(req.params.userId);
-    if(!user) {
-      res.status(404).send({message: 'Пользователь не найден'});
-      return;
-    }
-    res.status(200).send(user);
-  }
-  catch (err) {
-    if (err.kind === 'ObjectId') {
-      res.status(400).send({message: 'Введен неверный ID'});
-      return;
-    }
-    res.status(500).send({message: err.message});
-  }
+  Users.findById(req.params.userId)
+  .then(users => res.send({data: users}))
+  .catch(() => res.status(500).send({message: 'Произошла ошибка'}));
 };
 
 module.exports.createProfile = (req, res) => {
-  try {
-    const {name, about, avatar} = req.body;
-    const user = Users.create({name, about, avatar});
-    res.status(200).send(user);
-  }
-  catch (err) {
-    if (err.name === 'ValidatorError') {
-      res.status(400).send({message: 'Введены неверные данные'});
-      return;
-    }
-    res.status(500).send({message: err.message});
-  }
+  const {name, about, avatar} = req.body;
+  Users.create({name, about, avatar})
+  .then(users => res.send({data: users}))
+  .catch(() => res.status(500).send({message: 'Произошла ошибка'}));
 }
 
 module.exports.editProfileInformation = (req, res) => {
-  try {
-    const user = Users.findByIdAndUpdate(req.user._id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      res.status(404).send({message: 'Введен неверный ID'});
-      return;
-    }
-    res.status(200).send(user);
-  }
-  catch (err) {
-    if (err.name === 'ValidatorError') {
-      res.status(400).send({message: 'Введены неверные данные'});
-      return;
-    }
-    res.status(500).send({message: err.message});
-  }
+  Users.findByIdAndUpdate(req.user._id, req.body, {new: true, runValidators: true})
+  .then(users => res.send({data: users}))
+  .catch(() => res.status(500).send({message: 'Произошла ошибка'}));
 };
 
 module.exports.editProfileAvatar = (req, res) => {
-  try {
-    const user = Users.findByIdAndUpdate(req.user._id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      res.status(404).send({message: 'Введен неверный ID'});
-      return;
-    }
-    res.status(200).send(user);
-  }
-  catch (err) {
-    if (err.name === 'ValidatorError') {
-      res.status(400).send({message: 'Введены неверные данные'});
-      return;
-    }
-    res.status(500).send({message: err.message});
-  }
+  Users.findByIdAndUpdate(req.user._id, req.body, {new: true, runValidators: true})
+  .then(users => res.send({data: users}))
+  .catch(() => res.status(500).send({message: 'Произошла ошибка'}));
 };
